df7d8725a10be873b72cb5224755a2b5e30d75af
Ben <benjheng@gmail.com>
2015-12-20 02:45:34 +0800

    Add es6-promise polyfill
    Adds promise support for browsers that don't normally have support for
    it (ie. Internet Explorer version <12).

--- app/client/index.js
+++ app/client/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { render } from 'react-dom';
 import App from '../common/containers/App';
 
+require('es6-promise').polyfill();
 require('../common/stylesheets/main.scss');
 
 const rootElement = document.getElementById('root');
