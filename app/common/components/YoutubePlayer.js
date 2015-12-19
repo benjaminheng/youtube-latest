2aa2e4f41e8e70e5c2e2f65aa6599fd90e9507fd
Ben <benjheng@gmail.com>
2015-12-20 02:16:31 +0800

    Stop refreshes from adding to browser history
    We do this by replacing the entire iframe upon every refresh, instead of
    letting React do its thing and update the src attribute in the iframe's
    virtual DOM. Simply updating the src attribute will cause the previous
    src to be added to the browser history.

--- app/common/components/YoutubePlayer.js
+++ app/common/components/YoutubePlayer.js
@@ -3,14 +3,42 @@ import React, { Component } from 'react';
 export default class YoutubePlayer extends Component {
     constructor(props) {
         super(props);
+        this.iframe = this.createIframeTemplate();
+    }
+
+    createIframeTemplate() {
+        let iframe = document.createElement('iframe');
+        iframe.setAttribute('type', 'text/html');
+        iframe.setAttribute('width', '640');
+        iframe.setAttribute('height', '390');
+        iframe.setAttribute('allowfullscreen', '');
+        return iframe;
+    }
+
+    componentWillUpdate(nextProps) {
+        if (nextProps.url !== undefined) {
+            this.replaceIframe(nextProps.url);
+        }
+    }
+
+    // We replace the entire iframe instead of letting React do its thing and update
+    // the src attribute in the virtual DOM so that we don't keep adding to the page
+    // history. Updating the src of an iframe will cause the iframe's previous src
+    // to be added to the history, making it unpleasant for users to navigate back.
+    replaceIframe(url) {
+        let iframeWrapper = this.refs.iframeWrapper;
+        this.iframe.src = url;
+        while (iframeWrapper.firstChild) {
+            iframeWrapper.removeChild(iframeWrapper.firstChild);
+        }
+        iframeWrapper.appendChild(this.iframe);
     }
 
     render() {
         const { url } = this.props;
         return (
             <div className='youtube-player'>
-                <div className='iframe-wrapper'>
-                    <iframe type="text/html" width="640" height="390" src={url} allowFullScreen ></iframe>
+                <div className='iframe-wrapper' ref='iframeWrapper'>
                 </div>
             </div>
         );
