ef0d53c118b0d1913aaace21859e8b47c89aab6f
Ben <benjheng@gmail.com>
2015-12-19 22:19:37 +0800

    Change API key env variable to API_KEY

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 const config = {
     isProduction: process.env.NODE_ENV === 'production',
     port: process.env.PORT,
-    apiKey: process.env.YTLATEST_API_KEY,
+    apiKey: process.env.API_KEY,
     githubRepo: 'https://github.com/benjaminheng/youtube-latest'
 }
 
